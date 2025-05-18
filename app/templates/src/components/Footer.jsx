b8479f10c87c7541db86cf4fa42b8e603237df51
JJ Juarez <julio.juarez5@outlook.com>
2025-05-18 14:31:48 -0500

    add period

--- app/templates/src/components/Footer.jsx
+++ app/templates/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => (
 <footer className="bg-blue-700 text-white py-6 px-4 text-center">
-    <p>&copy; {new Date().getFullYear()} YourBrand. All rights reserved. Created by <a href='https://www.bayoudevco.com' target='_blank'>Bayou Dev Co</a></p>
+    <p>&copy; {new Date().getFullYear()} YourBrand. All rights reserved. Created by <a href='https://www.bayoudevco.com' target='_blank'>Bayou Dev Co</a>.</p>
   </footer>
 );
 
